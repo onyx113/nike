b4cbf072f88bc378f8d832235d6ea3db82972e2b
onyx113 <mohsenwaller@gmail.com>
2025-07-10 09:29:16 +0330

    nike start

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    
+    
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
